cb898622b6e1f418881e11a51c38c4f7ee586a13
HIRONOBU <ttlatex@yahoo.co.jp>
2020-08-12 13:33:10 +0900

    初回追加

---
+++ BlazorApp1/Models/StringLengthCounterModel.cs
@@ -0,0 +1,27 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    /// <summary>
+    /// 文字列カウンタのモデル
+    /// </summary>
+    public class StringLengthCounterModel
+    {
+        public ReactivePropertySlim<string> Text1 { get; set; } = new ReactivePropertySlim<string>();
+
+        public ReadOnlyReactivePropertySlim<string> Text2 { get; set; }
+
+        public StringLengthCounterModel()
+        {
+            this.Text2 =
+                Text1
+                .Select(x => string.IsNullOrEmpty(x) ? "0文字" : x.Length + "文字")
+                .ToReadOnlyReactivePropertySlim();
+        }
+    }
+}
---
+++ BlazorApp1/ViewModels/StringLengthCounterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using BlazorApp1.Models;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.ViewModels
+{
+    /// <summary>
+    /// 文字列カウンタのビューモデル
+    /// </summary>
+    public class StringLengthCounterViewModel
+    {
+        public ReactivePropertySlim<string> Text1 { get; set; } = new ReactivePropertySlim<string>();
+
+        public ReadOnlyReactivePropertySlim<string> Text2 { get; set; }
+
+        public StringLengthCounterViewModel(StringLengthCounterModel model)
+        {
+            this.Text1 = model.Text1;
+            this.Text2 = model.Text2;
+        }
+    }
+}
